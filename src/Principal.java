5b3846b4f96b0a86702ddc33bf7ea7f7290b181c
Tecnico - TI <tecnico@epfserver.com.br>
2017-03-27 21:41:35 -0300

    Criação do Projeto com as classes principais

---
+++ src/Principal.java
@@ -0,0 +1,28 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		TV tv1 = new TV(12, 20, "Sony", true);
+
+		System.out.println("A TV está no canal " + tv1.getCanal());
+		System.out.println("O volume da TV é " + tv1.getVolume());
+		System.out.println("O modelo da TV é " + tv1.getModelo());
+		System.out.println("A TV está ligada? " + tv1.isLigada());
+
+		tv1.aumentarCanal();
+		System.out.println("A TV está no canal " + tv1.getCanal());
+		tv1.aumentarVolume();
+		System.out.println("O volume da TV é " + tv1.getVolume());
+		tv1.desligar();
+		System.out.println("A TV está ligada? " + tv1.isLigada());
+		tv1.ligar();
+		System.out.println("A TV está ligada? " + tv1.isLigada());
+		tv1.diminuirCanal();
+		System.out.println("A TV está no canal " + tv1.getCanal());
+		tv1.diminuirVolume();
+		System.out.println("O volume da TV é " + tv1.getVolume());
+		tv1.setModelo("Samsung");
+		System.out.println("Modelo alterado para " + tv1.getModelo());
+	}
+
+}
---
+++ src/TV.java
@@ -0,0 +1,56 @@
+
+public class TV {
+	private int canal, volume;
+	private String modelo;
+	private boolean ligada;
+
+	public TV(int canal, int volume, String modelo, boolean ligada) {
+		this.canal = canal;
+		this.volume = volume;
+		this.modelo = modelo;
+		this.ligada = ligada;
+	}
+
+	public int getCanal() {
+		return canal;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public boolean isLigada() {
+		return ligada;
+	}
+
+	public void aumentarCanal() {
+		this.canal++;
+	}
+
+	public void diminuirCanal() {
+		this.canal--;
+	}
+
+	public void aumentarVolume() {
+		this.volume++;
+	}
+
+	public void diminuirVolume() {
+		this.volume--;
+	}
+
+	public void ligar() {
+		this.ligada = true;
+	}
+
+	public void desligar() {
+		this.ligada = false;
+	}
+	public void setModelo (String modelo){
+		this.modelo = modelo;
+	}
+}
